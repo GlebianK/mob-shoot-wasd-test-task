b7de81b633704d08a2805377aee197aa9b015528
glebianK <glebian001@gmail.com>
2025-09-02 18:48:57 +0300

    Player movement and rotation
    - Игрок перемещается с помощью WASD (UI-кнопки пока не готовы)
    - Игрок вращается через дрэг мыши (и, возможно, тач скрин, но это не точно)
    - FIX: есть инверсия вращения (например, когда игрок повёрнут на 180 и мышкой двигаем слева)

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float playerSideSpeed = 5.5f;
+    [SerializeField] private float playerRotationSpeed = 5.5f;
+
+    private Vector2 direction;
+    private Vector2 dragOrTouchDelta;
+
+    private void Start()
+    {
+        direction = Vector2.zero;
+        dragOrTouchDelta = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (direction != Vector2.zero)
+            rb.linearVelocity = direction * (playerSideSpeed * Time.deltaTime);
+        else
+            rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, Vector2.zero, 0.5f);
+    }
+
+    private void RotatePlayer()
+    {
+        if (dragOrTouchDelta != Vector2.zero)
+        {
+            float angleInDergrees = dragOrTouchDelta.y != 0 ? dragOrTouchDelta.y : dragOrTouchDelta.x;
+            transform.Rotate(Vector3.forward, angleInDergrees * playerRotationSpeed * Time.deltaTime);
+        }
+    }
+
+    #region INPUT CALLBACKS
+
+    public void OnMove(InputAction.CallbackContext callback)
+    {
+        if (callback.performed)
+        {
+            direction = callback.ReadValue<Vector2>();
+            //Debug.Log($"OnMove callback. Value = {direction}");
+        }
+        else if (callback.canceled)
+        {
+            direction = Vector2.zero;
+            //Debug.LogWarning("OnMove callback. Movement canceled!");
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext callback)
+    {
+        dragOrTouchDelta = callback.ReadValue<Vector2>();
+        Debug.Log($"OnLook callback. Value = {dragOrTouchDelta}");
+
+        //RotatePlayer();
+    }
+
+    #endregion
+}
