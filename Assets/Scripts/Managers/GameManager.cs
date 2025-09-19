0ebf482f26268e7e792848090bdc169c43b65d17
glebianK <glebian001@gmail.com>
2025-09-19 19:42:31 +0300

    PC gameplay is ready
    - Не проверял мобилку
    - Не балансил после ПК-билда, пок атолько через симуляцию всё проверял
    - Нет звуков

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -35,7 +35,7 @@ public class GameManager : MonoBehaviour
         totalEnemiesToKill = 0;
         kills = 0;
 
-        Application.targetFrameRate = 60; // TODO: убрать в билде!
+        //Application.targetFrameRate = 60; // TODO: убрать в билде!
 
         switch (SystemInfo.deviceType)
         {
--- Assets/Scripts/UI/VictoryCanvas.cs
+++ Assets/Scripts/UI/VictoryCanvas.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 public class VictoryCanvas : MonoBehaviour
 {
     [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text hpText;
 
     private void OnEnable()
     {
         if (GameManager.Instance == null)
             return;
 
-        int timer = (int)GameManager.Instance.GameTimer;
-        Debug.LogWarning($"Timer: {timer}");
+        float timer = GameManager.Instance.GameTimer;
+        timerText.text = "Your time: " + timer.ToString("F1") + " seconds";
+
+        GameObject player = FindAnyObjectByType<PlayerControl>().gameObject;
+        if (player.TryGetComponent<Health>(out Health playerHealth))
+        {
+            float temp = playerHealth.HP;
+            hpText.text = "Your HP: " + temp.ToString("0");
+        }
+        else
+        {
+            hpText.text = "Your HP: Not Found";
+        }
     }
 }
